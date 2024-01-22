4bda5c2621ff0d62afcd8fcf89971f8cb4a95b97
Rushabh Aradwad <aradwadrushabh@gmail.com>
2024-01-22 19:05:42 +0530

    start prod script upgrade

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,12 +1,33 @@
+/* eslint-disable no-undef */
 const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
+const path = require("path");
 app.use(bodyParser.json());
+
+//Use EJS as view engine
+app.set("view engine","ejs");
+
+
+app.get("/",async (request,response)=>{
+  const allTodos = await Todo.getTodos();
+  if(request.accepts("html")){
+    response.render('index',{
+      allTodos
+    });
+  } else{
+    response.json({
+      allTodos
+    })
+  } 
+});
 
 app.get("/", function (request, response) {
   response.send("Hello World");
 });
+
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get("/todos", async function (_request, response) {
   console.log("Processing list of all Todos ...");
